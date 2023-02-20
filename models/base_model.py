9a57aa223b849a67629ce289838a1c9ffcd55ac8
adamdiallo2k <adamdiallo2k@gmail.com>
2023-02-20 06:15:32 -0800

    task 3

---
+++ models/base_model.py
@@ -0,0 +1,25 @@
+#!/usr/bin/python3
+import datetime
+import uuid
+
+class BaseModel:
+    id = str(uuid.uuid4())
+    created_at = datetime.datetime.now()
+    updated_at = datetime.datetime.now()
+    
+    def __init__(self):
+        self.id = BaseModel.id
+        self.created_at = BaseModel.created_at
+        self.updated_at = BaseModel.created_at
+    
+    def __str__(self):
+        return "f{type(self).__name__} ({<self.id>}) {self.__dict__}"
+
+    def save(self):
+        self.updated_at = datetime.datetime.now()
+    
+    def to_dict(self):
+        self.__dict__ = {"__class__": "f{type(self).__name__}"}
+        self.__dict__["created_at"] : {"f{self.created_at.isoformat()}"}
+        self.__dict__["updated_at"] : {"f{self.updated_at.isoformat()}"}
+        return self.__dict__

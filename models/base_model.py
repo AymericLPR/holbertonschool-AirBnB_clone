38fc8f566eaef346c241100fcf513e9d1a3ab973
adamdiallo2k <adamdiallo2k@gmail.com>
2023-02-21 03:41:30 -0800

    test

--- models/base_model.py
+++ models/base_model.py
@@ -4,20 +4,24 @@ import datetime
 import uuid
 
 class BaseModel:
-    
+""" comment the base Model """    
     created_at = datetime.datetime.now()
     updated_at = datetime.datetime.now()
     
     def __init__(self):
+        """ coments """
         self.id = str(uuid.uuid4())
     
     def __str__(self):
+        """ comments """
         return f"[{type(self).__name__}] ({self.id}) {self.__dict__}"
     
     def save(self):
+        """ comments """
         self.updated_at = datetime.datetime.now()
     
     def to_dict(self):
+        """ comments """
         obj_dict = self.__dict__.copy()
         obj_dict["__class__"] = type(self).__name__
         obj_dict["created_at"] = self.created_at.isoformat()

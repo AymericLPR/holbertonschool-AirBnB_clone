a16f3eafab211cb801ed4c99f3eb4f1816c49e77
adamdiallo2k <adamdiallo2k@gmail.com>
2023-02-21 02:18:19 -0800

    fix

--- models/base_model.py
+++ models/base_model.py
@@ -17,7 +17,8 @@ class BaseModel:
         self.updated_at = datetime.datetime.now()
     
     def to_dict(self):
-        self.__dict__ = {"__class__": "f{type(self).__name__}"}
-        self.__dict__["created_at"] : {"f{self.created_at.isoformat()}"}
-        self.__dict__["updated_at"] : {"f{self.updated_at.isoformat()}"}
-        return self.__dict__
+        obj_dict = self.__dict__.copy()
+        obj_dict["__class__"] = type(self).__name__
+        obj_dict["created_at"] = self.created_at.isoformat()
+        obj_dict["updated_at"] = self.updated_at.isoformat()
+        return obj_dict

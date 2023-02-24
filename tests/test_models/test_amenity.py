dcf15b1644001f2b6fc11001c5f7786254782128
AymericLPR <mric31400@gmail.com>
2023-02-24 11:34:27 -0800

    simply test + authors

--- tests/test_models/test_amenity.py
+++ tests/test_models/test_amenity.py
@@ -1,55 +1,16 @@
 #!/usr/bin/python3
-"""test for amenity"""
+""" Modules for Amenity unit tests"""
 import unittest
-import os
-from models.amenity import Amenity
 from models.base_model import BaseModel
-from models.engine.file_storage import FileStorage
+from models.amenity import Amenity
 
 
 class TestAmenity(unittest.TestCase):
-    """test the Amenity class"""
-
-    def setUp(self):
-        """Sets up test methods."""
-        FileStorage._FileStorage__file_path = "test_json"
-        self.amenity = Amenity()
-        self.amenity.name = "Whatever"
-        self.amenity.save()
-
-    def test_docstring_Amenity(self):
-        """checking for docstrings"""
-        self.assertIsNotNone(Amenity.__doc__)
-
-    def test_attributes_Amenity(self):
-        """chekcing if amenity have attibutes"""
-        self.assertTrue('id' in self.amenity.__dict__)
-        self.assertTrue('created_at' in self.amenity.__dict__)
-        self.assertTrue('updated_at' in self.amenity.__dict__)
-        self.assertTrue('name' in self.amenity.__dict__)
-        self.assertEqual(hasattr(self.amenity, "name"), True)
-
-    def test_subclass_Amenity(self):
-        """test if Amenity is subclass of Basemodel"""
-        self.assertTrue(issubclass(self.amenity.__class__, BaseModel), True)
-        self.assertIsInstance(self.amenity, Amenity)
-
-    def test_type_Amenity(self):
-        """test attribute type for Amenity"""
-        self.assertEqual(type(self.amenity.name), str)
-
-    def test_save_Amenity(self):
-        """test if the save works"""
-        self.amenity.save()
-        self.assertNotEqual(self.amenity.created_at, self.amenity.updated_at)
-
-    def test_to_dict_Amenity(self):
-        """test if dictionary works"""
-        self.assertEqual('to_dict' in dir(self.amenity), True)
-
-    def tearDown(self):
-        os.remove(FileStorage._FileStorage__file_path)
+    """ tests Amenity """
+    def test_city(self):
+        """ checks Amenity's attributes """
+        self.assertEqual(Amenity.name, "")
 
 
-if __name__ == "__main__":
-    unittest.main()
+if __name__ == '__main__':
+    unittest.main
